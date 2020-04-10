c4a2e47c5db96fdb532c5714c2e31eb71ee5d0b3
lsiewers <luuksiewers@icloud.com>
2020-04-10 15:50:38 +0200

    Hide api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ export const environment = {
 
   // Your web app's Firebase configuration
   firebase: {
-    apiKey: "AIzaSyC6OeCoW4P2U_8SiXVYfXDbItwjZbMQDZk",
-    authDomain: "portfolio-ebc65.firebaseapp.com",
-    databaseURL: "https://portfolio-ebc65.firebaseio.com",
-    projectId: "portfolio-ebc65",
-    storageBucket: "portfolio-ebc65.appspot.com",
-    messagingSenderId: "775740224672",
-    appId: "1:775740224672:web:e37613db65c88bf72c05c5",
-    measurementId: "G-85J4E7QM07"
+    apiKey: 'AIzaSyC6OeCoW4P2U_8SiXVYfXDbItwjZbMQDZk',
+    authDomain: 'portfolio-ebc65.firebaseapp.com',
+    databaseURL: 'https://portfolio-ebc65.firebaseio.com',
+    projectId: 'portfolio-ebc65',
+    storageBucket: 'portfolio-ebc65.appspot.com',
+    messagingSenderId: '775740224672',
+    appId: '1:775740224672:web:e37613db65c88bf72c05c5',
+    measurementId: 'G-85J4E7QM07'
   }
-  
+
 };
